71827eed142f8afec146505177630efd51222551
Aradhya89 <aradhyagupta637@gmail.com>
2025-09-22 22:31:02 +0530

    definig some functions

--- hotelmanagement.py
+++ hotelmanagement.py
@@ -8,10 +8,8 @@ import pyttsx3
 #     def welcome_voice(self):
 #         self.engine.say("Welcome to the Restaurants")
 #         self.engine.runAndWait()
-# cur.execute("create table if not exists price(item_code int  ,item_name char(25) not null ,price int not null, foreign key (item_code) references menu (item_code));")
+            # resturant.cur.execute("create table if not exists sale(order_no int primary key ,date char(15) not null ,time char(15) not null, foreign key (item_code) references menu (item_code));")
 # cur.execute("select * from info")
-# a = cur.fetchall()
-# print(a)
 
 # creating parent class resturant
 class resturant:
@@ -26,7 +24,7 @@ class resturant:
     class menu ():
         
         def __init__(self):
-            resturant.cur.execute("create table if not exists menu(item_code int primary key ,item_name char(25) not null ,price int not null);" ) #making table if not exits
+            resturant.cur.execute("create table if not exists menu(item_code int primary key ,item_name char(25) not null ,item_price int not null);" ) #making table if not exits
             
         class menu_update:
             def __init__(self):
@@ -42,12 +40,15 @@ class resturant:
             def item_code(self,itemcode,newvalue):
                 resturant.cur.execute(f"update menu set item_code = {newvalue} where item_code = {itemcode}")
                 
-
-            def itme_insert(self,item_code,item_name,item_price):
-                resturant.cur.execute(f"insert into table menu values ({item_code},{item_name},{item_price})")
-
-            def item_delete(self):
-                resturant.cur.execute(f"")
+        
+            def item_insert(item_code,item_name,item_price):
+                try: #using try block to avoide duplicate entries error
+                    resturant.cur.execute(f"insert into menu values ({item_code},\"{item_name}\",{item_price})")
+                except ms.errors.IntegrityError:
+                    print("ENTERED SAME ITEM_CODE PRESENT IN TABLE")
+            
+            def item_delete(item_code):
+                resturant.cur.execute(f"delete from menu where item_code = {item_code}")
 
         def __str__(self):
                 resturant.cur.execute("select * from menu")
@@ -55,6 +56,8 @@ class resturant:
                 return tabulate.tabulate(data,["ITEM CODE","ITEM NAME","ITEM PRICE"],tablefmt= "grid")
 
     class sales:
+        def __init__(self):
+            resturant.cur.execute("create table if not exists sale(order_no int primary key ,date char(15) not null ,time char(15) not null, total_sale int not null);")
         def print_sale():
             pass
         def print_analysis():
@@ -69,15 +72,55 @@ class resturant:
                 pass
 
     class bill:
+        order = {}
+
         def generate():
             pass
-        def print():
-            pass
-        def adding_items():
-            pass
-        def removing_items():
-            pass
+
+        def __str__(self): #to print direct bill
+            return (tabulate.tabulate(self.order.items(),["ITEM NAME","ITEM PRICE"],tablefmt= "grid",missingval= ""))
+        
+
+        @staticmethod
+        def gettingitemdetail(item_code) -> int | str:
+            resturant.cur.execute(f"select item_price,item_name from menu where item_code = {item_code}")
+            data = resturant.cur.fetchall()
+            itemprice = data[0][0]  #data[0][1] = item_price
+            itemname  = data [0][1]  #the reson behind it was the output was [(itemprice,itemname)]
+            return itemprice,itemname
+
+
+        @classmethod
+        def adding_items(cls,item_code, quantity:int):
+            price,name = resturant.bill.gettingitemdetail(item_code)
+            total_price = price * quantity 
+            cls.order[name] = total_price
+
+        @classmethod
+        def removing_items(cls,item_code,quantity:int) -> None:
+            try:
+                price , name = resturant.bill.gettingitemdetail(item_code)
+                removing_price = price * quantity
+                if (calculated_price := resturant.bill.order[name]- removing_price) >0:
+                    cls.order[name] = calculated_price
+                else:
+                    del cls.order[name]
+            except KeyError:
+                print("ITEM NOT IN BILL :)")
+
 
     # a = sfx()
     # a.welcome_voice(
-print(resturant.menu())
+# print(resturant.menu())
+# shriji = resturant()
+# bill1 = shriji.bill
+# bill1.adding_items(101,2)
+# print(bill1(),"\n")
+# bill1.removing_items(101,1)
+# print(bill1(),"\n")
+# bill1.removing_items(101,1)
+# print(bill1())
+# # a.bill.adding_items(101,2)
+# # a.menu.menu_update.item_insert(101,"samosa",20)
+# resturant.con.commit()
+# # print(a.bill())
