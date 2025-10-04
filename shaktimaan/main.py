83207df4617efc7c564761040564cc169e6b9568
Aradhya89 <aradhyagupta637@gmail.com>
2025-10-04 22:27:18 +0530

    adding ai response to it

--- shaktimaan/main.py
+++ shaktimaan/main.py
@@ -4,17 +4,23 @@ import pyttsx3
 import pygame 
 import sys
 from listoflist import *
+from google import genai
+import os
+import dotenv
+
+
 # import openai
-# import dotenv
-# import os
 
-
-# dotenv.load_dotenv() # to add api key in env to os.env
+#for openai 
 # openaiapi = os.environ.get("openai_api")
 # client = openai.OpenAI(api_key= openaiapi)
 
-#initializing module
+# for google api
+dotenv.load_dotenv() # to add api key in env to os.env
 
+
+
+#initializing module
 recogniser = sr.Recognizer()
 tt = pyttsx3.init()
 pygame.mixer.init()
@@ -24,9 +30,25 @@ def voicechange(x):
     voices = tt.getProperty("voices")
     tt.setProperty('voice',voices[x].id)   
 
+def genaibrain(a:str) -> str:
+    with genai.Client(api_key=os.environ.get("GEMINI_API_KEY")) as client:
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=f"{a} in very short (like 15 words)",
+            #writing this to reduce token usage
+            config=genai.types.GenerateContentConfig(
+                thinking_config=genai.types.ThinkingConfig(thinking_budget=0) # Disables thinking
+            ),
+        )
+        return (response.text)
+    
 
-tt.say("initializing shaktimaan")
-tt.runAndWait()
+def speak(a):
+    tt.say(a)
+    tt.runAndWait()
+
+            
+speak("initializing shaktimaan")
 
 if __name__ == "__main__": #writig this so it will not work after importing on other
 
@@ -44,7 +66,7 @@ if __name__ == "__main__": #writig this so it will not work after importing on o
         try:
             #code for waking up saktimaan
             if (recogniser.recognize_google(entry).lower()) == "shaktiman":
-                pygame.mixer.Sound("shaktimaan/sfx/game-start-317318.mp3").play()
+                pygame.mixer.Sound("Projects-Python/shaktimaan/sfx/game-start-317318.mp3").play()
                 print("\nshaktiman is listening")
                 
                 with sr.Microphone() as source:
@@ -52,25 +74,35 @@ if __name__ == "__main__": #writig this so it will not work after importing on o
                     print("saktimaan is processing the text")
                 try:
                     print("\nyou said :",a:= recogniser.recognize_google(audio))
-                    if 'open' in a.lower():
+
+
                     # checking any link word are in sentence if yes then opening it
+                    if 'open' in a.lower():
                         for b in list(linklist.keys()):
                             if b in a.lower() :
                                 web.open(linklist[b])
-                                pygame.mixer.Sound("shaktimaan/sfx/okkk.mp3").play()
+                                pygame.mixer.Sound("Projects-Python/shaktimaan/sfx/okkk.mp3").play()
                                 break
-                    if 'play' in a.lower():
+                            # else:
+                            #     print("no link founded")
+                            #     break
+
+
+                    elif 'play' in a.lower():
                         for b in list(musiclist.keys()):
                             if b in a.lower() :
                                 web.open(musiclist[b])
-                                pygame.mixer.Sound("shaktimaan/sfx/okkk.mp3").play()
+                                pygame.mixer.Sound("Projects-Python/shaktimaan/sfx/okkk.mp3").play()
                                 break
                         
                     
                     #command for exiting while loop
-                    if a.lower() == "system shutdown" :
+                    elif a.lower() == "system shutdown" :
                         sys.exit()
-                    else :...
+
+
+                    else :
+                        print(data :=genaibrain(a))
                         # try:
                         #     client.chat.completions.create(model= "gpt-3.5-turbo",
                         #         messages= [{'role':"system","content":"your are a ai system of automated project like jarvis"},
